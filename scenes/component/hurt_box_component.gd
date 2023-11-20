062870a265d54ffa0bac6bd8d94db491fa586c42
Marko Ristic <marko.reestic@gmail.com>
2023-11-20 23:15:49 +0100

    Damage text

--- scenes/component/hurt_box_component.gd
+++ scenes/component/hurt_box_component.gd
@@ -4,6 +4,8 @@ class_name HurtBoxComponent
 
 @export var health_cpomoenent: Node
 
+var floating_text_scene = preload("res://scenes/ui/floating_text.tscn")
+
 
 func _ready():
 	area_entered.connect(ona_area_entered)
@@ -13,5 +15,10 @@ func ona_area_entered(other_area:Area2D):
 	if not other_area is HitBoxComponent: return
 	if health_cpomoenent == null: return
 	
-	var hit_vox_cpomonent = other_area as HitBoxComponent
-	health_cpomoenent.damage(hit_vox_cpomonent.damage)
+	var hit_box_cpomonent = other_area as HitBoxComponent
+	health_cpomoenent.damage(hit_box_cpomonent.damage)
+
+	var floating_text = floating_text_scene.instantiate() as Node2D
+	get_tree().get_first_node_in_group("foreground_layer").add_child(floating_text)
+	floating_text.global_position = global_position + Vector2.UP * 16
+	floating_text.start(str(hit_box_cpomonent.damage))
